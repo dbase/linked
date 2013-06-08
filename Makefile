89a9e4a6f9218e6295b0dcda81e5a6b355acbc80
cyphar <cyphar@cyphar.com>
2013-06-09 03:41:45 +1000

    added tests and fixed length not being updated

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
-all:
-	@echo "Coming Soon"
+CC	?= gcc
+
+CFLAGS	= -Isrc/ -g -O0
+LFLAGS	=
+WARN	= -Wall -Wextra -Werror
+
+SRC	= src/linked.c tests/test.c
+DEPS	= src/linked.h
+
+NAME	= test
+
+test: $(SRC) $(DEPS)
+	$(CC) $(WARN) $(CFLAGS) $(LFLAGS) $(SRC) -o $(NAME)
---
+++ tests/test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <linked.h>
+
+int main(void) {
+	link_t *table = link_init();
+	link_iter *ii = NULL;
+
+	/* generate a list */
+	printf("generating list\n");
+	int to = 100;
+	while(to--)
+		link_append(table, &to, sizeof(int));
+
+	printf("print list of length %d\n", table->length);
+	ii = link_iter_init(table);
+	do {
+		if(ii->content)
+			printf("%d ", *(int *) ii->content);
+	} while(!link_iter_next(ii));
+	printf("\n");
+	free(ii);
+
+	printf("shorten list by 25\n");
+	link_shorten(table, 25);
+
+	printf("print list of length %d\n", table->length);
+	ii = link_iter_init(table);
+	do {
+		if(ii->content)
+			printf("%d ", *(int *) ii->content);
+	} while(!link_iter_next(ii));
+	printf("\n");
+	free(ii);
+
+	printf("truncate list to 56\n");
+	link_truncate(table, 56);
+
+	printf("print list of length %d\n", table->length);
+	ii = link_iter_init(table);
+	do {
+		if(ii->content)
+			printf("%d ", *(int *) ii->content);
+	} while(!link_iter_next(ii));
+	printf("\n");
+	free(ii);
+
+	link_free(table);
+	return 0;
+}
